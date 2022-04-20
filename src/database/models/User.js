283f35148c13ded3a7cc51eb7449e2a8ed7e9b18
mthlopes <matheus.lopes.souza@hotmail.com>
2022-04-20 20:07:28 -0300

    :bug: Arrumando Require

--- src/database/models/User.js
+++ src/database/models/User.js
@@ -2,13 +2,13 @@ const mongoose = require('mongoose');
 
 const User = new mongoose.Schema({
     usuario: { type:String, required:true, trim:true },
-    nome: { type:String, require:true },
+    nome: { type:String, required:true },
     senha: { type:String, required:true },
     lists:[new mongoose.Schema({
-        nome: { type:String, require:true },
+        nome: { type:String, required:true },
         tasks: [new mongoose.Schema({
-            isCheck: { type:Boolean, require:true, default:false },
-            dc: { type:String, require:true },
+            isCheck: { type:Boolean, required:true, default:false },
+            dc: { type:String, required:true },
         })],
     })],
 });
